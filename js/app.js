e45ed106199c24d177b7898bc6bc6e469862426f
enod <win_1118@hotmail.com>
2022-07-23 10:48:29 -0500

    Added the full if statements for the possible outcomes, though it just returns who won each round, still needs to increment and decrement the points earn for each round, thinking about the scope to use for that variable

--- js/app.js
+++ js/app.js
@@ -11,9 +11,29 @@ function getPlayerChoice(){
 }
 
 function playRound(){
-    const player = getPlayerChoice()
-    const computer = getComputerChoice()
-    console.log(player,computer)
-    return
+    
+    const playerSelection = getPlayerChoice()
+    const computerSelection = getComputerChoice()
+    
+    if(playerSelection == "piedra" && computerSelection == "piedra"){
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    } else if(playerSelection == "piedra" && computerSelection == "tijeras"){
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "piedra" && computerSelection == "papel"){
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "papel" && computerSelection == "piedra"){
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "papel" && computerSelection == "papel"){
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "papel" && computerSelection == "tijeras"){
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "tijeras" && computerSelection == "piedra"){
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "tijeras" && computerSelection == "papel"){
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }else if(playerSelection == "tijeras" && computerSelection == "tijeras"){
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+    }
+
 }
 
