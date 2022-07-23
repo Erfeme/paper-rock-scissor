a7ed87161ce5261a1bcb2801e3d4be69aea5490f
enod <win_1118@hotmail.com>
2022-07-23 10:49:43 -0500

    Added an increment to variables representing each player points, when the for loops ends and it returns to the flux of the game function it will determinates who won

--- js/app.js
+++ js/app.js
@@ -11,29 +11,44 @@ function getPlayerChoice(){
 }
 
 function playRound(){
-    
+
     const playerSelection = getPlayerChoice()
     const computerSelection = getComputerChoice()
     
     if(playerSelection == "piedra" && computerSelection == "piedra"){
         return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     } else if(playerSelection == "piedra" && computerSelection == "tijeras"){
+        playerPoints=+1;
         return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "piedra" && computerSelection == "papel"){
+        computerPoints=+1;
         return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "papel" && computerSelection == "piedra"){
+        playerPoints=+1;
         return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "papel" && computerSelection == "papel"){
         return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "papel" && computerSelection == "tijeras"){
+        computerPoints=+1;
         return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "tijeras" && computerSelection == "piedra"){
+        computerPoints =+1;
         return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "tijeras" && computerSelection == "papel"){
+        playerPoints =+ 1;
         return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }else if(playerSelection == "tijeras" && computerSelection == "tijeras"){
         return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
     }
-
 }
 
+function game(){
+
+    let computerPoints = 0;
+    let playerPoints = 0;
+
+    for (let i = 0; i < 5; i++){
+
+    }
+
+}
