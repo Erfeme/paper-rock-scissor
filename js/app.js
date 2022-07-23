be242fe313ef694c22f2e8f8ceb693eb4be680e8
enod <win_1118@hotmail.com>
2022-07-23 10:19:11 -0500

    Added the getPlayerChoice function, it asks in a prompt to the player to write paper, rock or scissors, it returns the string in lowercase

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ function getComputerChoice(){
     items = ["piedra","papel","tijeras"];
     return items[seleccion]
 }
+
+function getPlayerChoice(){
+    let playerSelection = prompt('Escribe Piedra, papel o tijeras').toLowerCase();
+    return playerSelection;
+}
