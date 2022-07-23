41e1fb96fbfa93eaf3bb727473181d39cc0b3201
enod <win_1118@hotmail.com>
2022-07-23 11:01:10 -0500

    Tested the game function, it works, it returns the desired data in a string, but had problems with scope of playerPoints and computerPoints variables, decided to make them global so it don't display errors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let computerPoints = 0;
+let playerPoints = 0;
+
 function getComputerChoice(){
     let seleccion = Math.floor((Math.random() * 3));
 
@@ -16,39 +19,37 @@ function playRound(){
     const computerSelection = getComputerChoice()
     
     if(playerSelection == "piedra" && computerSelection == "piedra"){
-        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     } else if(playerSelection == "piedra" && computerSelection == "tijeras"){
         playerPoints=+1;
-        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "piedra" && computerSelection == "papel"){
         computerPoints=+1;
-        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "papel" && computerSelection == "piedra"){
         playerPoints=+1;
-        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "papel" && computerSelection == "papel"){
-        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "papel" && computerSelection == "tijeras"){
         computerPoints=+1;
-        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "tijeras" && computerSelection == "piedra"){
         computerPoints =+1;
-        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "tijeras" && computerSelection == "papel"){
         playerPoints =+ 1;
-        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }else if(playerSelection == "tijeras" && computerSelection == "tijeras"){
-        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection}`)
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
     }
 }
 
 function game(){
 
-    let computerPoints = 0;
-    let playerPoints = 0;
-
+    
     for (let i = 0; i < 5; i++){
-
+        console.log(playRound());
     }
 
 }
