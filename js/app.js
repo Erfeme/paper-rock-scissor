7fae17ae541225e464c25b5bbb2e09a14749cf6f
enod <win_1118@hotmail.com>
2022-07-23 11:51:40 -0500

    Added semicolons to the code

--- js/app.js
+++ js/app.js
@@ -15,33 +15,33 @@ function getPlayerChoice(){
 
 function playRound(){
 
-    const playerSelection = getPlayerChoice()
-    const computerSelection = getComputerChoice()
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
     
     if(playerSelection == "piedra" && computerSelection == "piedra"){
-        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     } else if(playerSelection == "piedra" && computerSelection == "tijeras"){
         playerPoints=+1;
-        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "piedra" && computerSelection == "papel"){
         computerPoints=+1;
-        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "papel" && computerSelection == "piedra"){
         playerPoints=+1;
-        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "papel" && computerSelection == "papel"){
-        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "papel" && computerSelection == "tijeras"){
         computerPoints=+1;
-        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "tijeras" && computerSelection == "piedra"){
         computerPoints =+1;
-        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Gana la computadora! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "tijeras" && computerSelection == "papel"){
         playerPoints =+ 1;
-        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Gana el jugador! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }else if(playerSelection == "tijeras" && computerSelection == "tijeras"){
-        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`)
+        return(`Empate! la computadora ha seleccionado ${computerSelection} y el jugador ha seleccionado ${playerSelection} \n Computadora ${computerPoints} Jugador ${playerPoints}`);
     }
 }
 
@@ -53,9 +53,9 @@ function game(){
     }
 
     if (playerPoints > computerPoints){
-        return "The Player wins"
+        return "The Player wins";
     } else if(playerPoints<computerPoints){
-        return "The Computer wins"
+        return "The Computer wins";
     } else if(playerPoints == computerPoints){
         return "Tie"
     }
